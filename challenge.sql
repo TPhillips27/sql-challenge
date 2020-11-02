1c6f23dc9d1db93bb129bd1a17bb5af37b32a281
TPhillips27 <69609977+TPhillips27@users.noreply.github.com>
2020-11-01 21:44:01 -0500

    update

--- challenge.sql
+++ challenge.sql
@@ -62,3 +62,66 @@ CREATE TABLE "Dept_Manager" (
 );
 
 Select * from "Dept_Manager";
+
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM "Employees" AS e
+JOIN "Salaries" AS s
+ON e.emp_no = s.emp_no
+ORDER BY emp_no
+
+--List first name, last name, and hire date for employees who were hired in 1986.
+
+Select
+e.first_name,
+e.last_name,
+e.hire_date
+from "Employees" as e
+WHERE DATE_PART('year',hire_date) = 1986
+
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+
+SELECT dm.dept_no, 
+dm.dept_name,
+dm.emp_no,
+e.last_name, 
+e.first_name,
+FROM "Dept_Manager" as dm
+JOIN "Departments" as d
+on dept_manager.dept_no = departments.dept_no
+JOIN "Employees" as e
+on dept_manager.emp_no = employees.emp_no
+ORDER BY emp_no;
+
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+Select 
+e.emp_no,
+e.last_name,
+e.first_name,
+d.dept_name
+from "Employees" as e
+join "Departments" as d
+on d.emp_no = e.emp_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT * FROM "Employees"
+WHERE first_name = 'Hercules' AND last_name like 'B%';
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+
+
+--List all employees in the Sales and Development departments, 
+--including their employee number, last name, first name, and department name.
+
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+
+
